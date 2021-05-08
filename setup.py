a9faa0a987c5f805072fed5fc70ade5bd275d144
Marc Betoule <marc.betoule@lpnhe.in2p3.fr>
2021-05-08 10:32:15 +0200

    For unknown reasons setup not working without setuptools

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
-from distutils.core import setup
+<<<<<<< HEAD
+from setuptools import setup
+#from distutils.core import setup
+=======
+#from distutils.core import setup
+from setuptools import setup
+>>>>>>> f76651e (for some reason distutils is not working on this machine, switch to setup_tools)
 
 setup(name='qhpyccd',
       version="0.1",
       description='Python wrapper for the QHYCCD library',
       author='Emmanuel Bertin (IAP / SorbonneU)',
       author_email='bertin@iap.fr',
-      setup_requires=["cffi>=1.0.0", "libqhyccd>=21.0.0"],
+      setup_requires=["cffi>=1.0.0"],
       install_requires=["cffi>=1.0.0"],
       cffi_modules=["qhpyccd/qhpyccd.py:ffibuilder"],
       packages=['qhpyccd'],
